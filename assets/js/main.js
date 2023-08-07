9f0f30364790d3f3375598abf672ba367dfa1364
Anbu <15264938+anburocky3@users.noreply.github.com>
2023-08-07 18:35:31 +0530

    es6-variables

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+// If you did not define them, they would be assigned to the global object.
+// If you use var outside of a function, it belongs to the global scope.
+// If you use var inside of a function, it belongs to that function.
+// If you use var inside of a block, i.e. a for loop, the variable is still available outside of that block.
+
+// Let
+// Const
