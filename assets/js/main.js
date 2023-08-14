42f1013e5301ee903f1cd7e26d5c4d572778f168
Anbu <15264938+anburocky3@users.noreply.github.com>
2023-08-14 16:17:28 +0530

    02-recorded

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,61 @@
-// If you did not define them, they would be assigned to the global object.
-// If you use var outside of a function, it belongs to the global scope.
-// If you use var inside of a function, it belongs to that function.
-// If you use var inside of a block, i.e. a for loop, the variable is still available outside of that block.
+// Object
+const objName = new Object() // object constructor syntax
+const objName2 = {} // Object literal syntax
 
-// Let
-// Const
+// const personFirstName = 'Anbu'
+// const personLastName = 'Selvan'
+// const personAge = 27
+// const personFavColor = 'red'
+
+const person = {
+  firstName: 'Anbu',
+  lastName: 'Selvan',
+  age: 27,
+  isAlive: true,
+  parent: {
+    father: 'Appa',
+    mother: 'Amma',
+  },
+  fullName() {
+    return `${this.firstName} ${this.lastName}`
+  },
+}
+
+person.favColor = 'red'
+
+// getting object values
+// document.getElementById('result').innerHTML = person.fullName()
+
+// Object merging
+const personMethods = {
+  yearOfBirth() {
+    return new Date().getFullYear() - this.age
+  },
+  favNumbers: [1, 3, 5, 7, 9],
+  get favNumbersCount() {
+    return this.favNumbers.length
+  },
+}
+
+// Object Merging
+Object.assign(person, personMethods)
+
+// Object Cloning
+const objCopied = Object.assign({}, person)
+
+// Spread Operator
+const finalObj = { ...person, ...personMethods }
+
+// Delete
+delete person.age
+
+function getFullName(firstName, lastName) {
+  return {
+    firstName,
+    lastName,
+  }
+}
+
+console.log(getFullName('Anbu', 'Selvan'))
+
+console.log(person, person.favNumbersCount)
