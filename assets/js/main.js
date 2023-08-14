a54cdfb298f55d76ab405c4c84ddf9401331d9b7
Anbu <15264938+anburocky3@users.noreply.github.com>
2023-08-14 17:45:43 +0530

    rebase to old branch

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,12 @@
-// Object Destructuring
+// Object
+const objName = new Object() // object constructor syntax
+const objName2 = {} // Object literal syntax
+
+// const personFirstName = 'Anbu'
+// const personLastName = 'Selvan'
+// const personAge = 27
+// const personFavColor = 'red'
+
 const person = {
   firstName: 'Anbu',
   lastName: 'Selvan',
@@ -8,37 +16,46 @@ const person = {
     father: 'Appa',
     mother: 'Amma',
   },
-  fullName: function () {
-    return this.firstName + ' ' + this.lastName
+  fullName() {
+    return `${this.firstName} ${this.lastName}`
   },
 }
 
-// Default values
-const { firstName, lastName, favColor = 'red' } = person
+person.favColor = 'red'
 
-//// Assigning new variable names
-const {
-  firstName: myFirstName,
-  lastName: myLastName,
-  favColor = 'red',
-} = person
+// getting object values
+// document.getElementById('result').innerHTML = person.fullName()
 
-console.log(firstName, lastName, favColor)
-
-//// Object Destructuring and rest operator
-let favNum1, favNum2, rest
-;({ favNum1, favNum2, ...rest } = {
-  favNum1: 1,
-  favNum2: 3,
-  favNum3: 5,
-  favNum4: 7,
-})
-
-console.log(favNum1, favNum2, rest)
-
-// Destructing in Functions
-function changeFullName({ firstName, lastName }) {
-  console.log(firstName, lastName)
+// Object merging
+const personMethods = {
+  yearOfBirth() {
+    return new Date().getFullYear() - this.age
+  },
+  favNumbers: [1, 3, 5, 7, 9],
+  get favNumbersCount() {
+    return this.favNumbers.length
+  },
 }
 
-changeFullName(person)
+// Object Merging
+Object.assign(person, personMethods)
+
+// Object Cloning
+const objCopied = Object.assign({}, person)
+
+// Spread Operator
+const finalObj = { ...person, ...personMethods }
+
+// Delete
+delete person.age
+
+function getFullName(firstName, lastName) {
+  return {
+    firstName,
+    lastName,
+  }
+}
+
+console.log(getFullName('Anbu', 'Selvan'))
+
+console.log(person, person.favNumbersCount)
