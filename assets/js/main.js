ba7cdaecff59773e223bc51ffd00e9939480e7e2
Anbu <15264938+anburocky3@users.noreply.github.com>
2023-08-14 14:40:41 +0530

    07-plan

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,42 +1,63 @@
-// Array
-const dailySchedule = [
-  'wakeup',
-  'walking',
-  'breakfast',
-  'work',
-  'dinner',
-  'work',
-  'sleep',
+const fruits = ['Banana', 'Orange', 'Apple', 'Mango']
+
+// access array
+console.log(fruits[1])
+console.log(fruits.length) // get length of that array
+
+// // Array methods
+fruits.pop()
+fruits.push('Potato')
+
+console.log(fruits.shift())
+console.log(fruits.unshift('new-array'))
+
+// delete array
+delete fruits[1]
+
+// merging arrays
+const breakfast = ['idly', 'poori', 'vadai']
+const lunch = ['Chicken Biriyani']
+const dinner = ['parotta', 'grill-chicken']
+
+const overallFood = breakfast.concat(lunch, dinner, 'juice')
+
+console.log(overallFood)
+
+// Flattening a array
+const matrix = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
 ]
 
-// Doesn't have duplicate
-const dailyScheduleSet = new Set([
-  'wakeup',
-  'walking',
-  'breakfast',
-  'work',
-  'dinner',
-  'work',
-  'sleep',
-])
+console.log(matrix.flat()) // reduce dimensional array
 
-console.log(dailySchedule, dailyScheduleSet)
+// Splicing & Slicing methods
+console.log(fruits.slice(2, 3))
 
-// Properties
-console.log(dailyScheduleSet.size)
+console.log(fruits.splice(1, 0))
+// Array.reverse() or Array.toReversed()
+// Array.toSpliced()
+// Array.toSpliced()
 
-// Set Methods
-dailyScheduleSet.add('play')
-dailyScheduleSet.add({ meeting: ['friends'] })
+document.getElementById('result').innerHTML = fruits.toString()
+document.getElementById('result').innerHTML = fruits.join(' - ')
 
-console.log(dailyScheduleSet.has('work')) // to check, if i had worked.
+// // ES-Commonly used
+Array.map()
+Array.with()
+Array.sort() // Array.toSorted()
+Array.some()
+Array.reduce()
 
-dailyScheduleSet.delete('walking') // remove
+Array.from()
+Array.to()
+Array.copyWithin()
+Array.find()
+Array.findIndex()
+Array.entries()
+Array.keys()
+Array.values()
+Array.fill()
 
-// clear()	Removes all elements from a Set
-// forEach()	Invokes a callback for each element
-// values()	Returns an Iterator with all the values in a Set
-// keys()	Same as values()
-// entries()	Returns an Iterator with the [value,value] pairs from a Set
-
-console.log(dailyScheduleSet)
+Array.forEach()
