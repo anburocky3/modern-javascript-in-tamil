76ab2556c7029c2f3808222ef7c268b142bddb98
Anbu <15264938+anburocky3@users.noreply.github.com>
2023-08-21 20:16:59 +0530

    06-recorded

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,73 +1,57 @@
-// Javascript Map Object
-const foods = [
-  { id: 1, title: 'Chicken Biriyani', dietary: 'non-veg', title: "Mutton Biriyani"  },
-  { id: 2, title: 'Sambar', dietary: 'veg' },
-  { id: 3, title: 'Curd Rice', dietary: 'veg' },
+const dailySchedules = [
+  "wake up",
+  "exercise",
+  "breakfast",
+  "work",
+  "lunch",
+  "work",
+  "snacks",
+  "work",
+  "dinner",
+  "sleep",
 ]
 
-// Map
-const maagiIngredients = new Map()
-maagiIngredients.set("water", "1.5 Glass Cup")
-maagiIngredients.set("masala", 1)
-maagiIngredients.set('salt', '0.5 spoon')
-maagiIngredients.set('chilly', '1 spoon')
-maagiIngredients.set('chicken-masala', '1 spoon')
-// maagiIngredients.set({},'empty obj')
-// maagiIngredients.set(function(){}, 'empty function')
-// maagiIngredients.set(NaN, 'NaN')
+// // 
+const meetingObj = { 'meeting': ['friends']}
+const dailyScheduleSet = new Set(
+ [ "wake up",
+  "exercise",
+  "breakfast",
+  "work",
+  "lunch",
+  "work",
+  "snacks",
+  "work",
+  "dinner",
+  "sleep",]
+  )
+dailyScheduleSet.add("play")
+dailyScheduleSet.add(meetingObj)
 
-// or you can create like this.
-const func = function () {};
-const myMap = new Map([
-  [1, 'One'],
-  [2, 'Two'],
-  [func, 'empty obj']
-])
+console.log(dailySchedules, dailyScheduleSet);
 
-console.log('myMap', myMap.get(2));
+// Properties
+console.log(dailyScheduleSet.size)
 
-// How to access the map data
-console.log(maagiIngredients.get("salt"));
+// // Set Methods
+console.log(dailyScheduleSet.has('play'));
+console.log(dailyScheduleSet.entries()); 
+console.log(dailyScheduleSet.keys());
+console.log(dailyScheduleSet.values());
 
-// how to get all size of map
-console.log(maagiIngredients.size);
+dailyScheduleSet.delete(meetingObj)
+console.log('after deleting', dailyScheduleSet);
 
-// Map Methods
-console.log('Is sugar added?', maagiIngredients.has('sugar'));
-maagiIngredients.clear() // it removes all the keys and values pairs from the map object
-console.log(maagiIngredients.entries()); // return a new iterator object
-console.log(maagiIngredients.keys()); // it returns an iterator for all keys in the map.
-console.log(maagiIngredients.values()); // it returns an iterator for all values in the map.
+// clear()	Removes all elements from a Set
+// forEach()	Invokes a callback for each element
+// values()	Returns an Iterator with all the values in a Set
+// keys()	Same as values()
+// entries()	Returns an Iterator with the [value,value] pairs from a Set
 
-console.log(maagiIngredients);
+// example solution
+const resultOlEl = document.querySelector("#result")
 
-maagiIngredients.delete('chicken-masala') // it removes the specified key-pairs
-
-
-// displaying map data
-for(let [keyName, valueData] of maagiIngredients.entries()){
-  console.log(keyName, valueData);
-}
-
-// foreach
-maagiIngredients.forEach(ingredient => {
-  console.log('forEach ', ingredient);
+dailyScheduleSet.forEach(schedule => {
+  console.log(schedule);
+  resultOlEl.innerHTML += '<li>' + schedule + '</li>'
 })
-
-
-// Map Object - Object Key example
-const myEmptyObj = {}
-const exampleMap = new Map()
-exampleMap.set(myEmptyObj, 'empty Object')
-exampleMap.set('key', 'value')
-
-console.log(exampleMap.get(myEmptyObj));
-
-// WeakMap Object
-const myAnotherObj = {}
-const myWeakMap = new WeakMap()
-myWeakMap.set(myAnotherObj, 'Anbu')
-
-console.log(myWeakMap.get(myAnotherObj));
-
-
