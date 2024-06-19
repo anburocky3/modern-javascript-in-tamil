b72872a1608e9e6186e901beb0416b5e6fdef3e6
Anbuselvan Rocky <15264938+anburocky3@users.noreply.github.com>
2024-06-19 19:45:01 +0530

    12-post

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,23 +1,49 @@
-// ❌
-channel = 'CyberDude'
-
-var channel = 'CyberDude Networks'
-
-// ✅
-let channel = 'CyberDude'
-channel = 'CyberDude Networks'
-
-console.log(channel)
-
-// Function Hoisting
-sayHello() // can call from anywhere
-
-function sayHello() {
-  console.log('Hello CyberDude')
+// Pure Functions
+// add a function which will add points
+function addPoints(point) {
+  return point + 50
 }
 
-const sayHelloExpression = () => {
-  console.log('Hello CyberDude - Expression')
+const addedPoints = addPoints(50)
+const addedPoints2 = addPoints(10)
+const addedPoints3 = addPoints(10)
+
+console.log(addedPoints, addedPoints2, addedPoints3)
+
+function toUppercase(text) {
+  return text.toUpperCase()
 }
 
-sayHelloExpression() // should call only after declaration.
+const result = toUppercase('anbu') // => ANBU
+const result2 = toUppercase('trisha') // => TRISHA
+
+console.log(result, result2)
+
+// Impure Functions
+const foodList = ['Chicken Biriyani', 'Curd', 'Sambar']
+
+function addFoods(food) {
+  // return foodList.push(food)
+  return [...foodList, food]
+}
+
+const addedFoods = addFoods('Fried Rice')
+
+console.log('ORIGINAL', foodList)
+console.log('ADDED', addedFoods)
+
+/*
+- **Impure functions** behave differently:
+
+  - They can **modify the state of the application** or **have side effects**.
+
+  - Their behavior can be **unpredictable** and they may affect other parts of the application.
+
+- Characteristics of impure functions:
+
+  - They can modify state or have side effects.
+
+  - They depend on other parts of the code.
+
+  - Testing and maintenance are harder.
+*/
