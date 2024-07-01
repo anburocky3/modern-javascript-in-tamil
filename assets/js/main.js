6e32382843cbf88c7c33e16b68262b5632397a69
Anbuselvan Rocky <15264938+anburocky3@users.noreply.github.com>
2024-07-02 05:05:54 +0530

    21-post

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,25 +1,43 @@
-class Learner {
-  #pin
+class Vehicle {
+  // Private properties (conventionally use _ or #)
+  #speed
+  #fuel
 
-  constructor(name, age, pin) {
-    this.name = name
-    this.age = age
-    this.#pin = pin
-    this._protectedProperty = 'some value'
+  constructor(speed, fuel) {
+    this.#speed = speed
+    this.#fuel = fuel
   }
 
-  bio() {
-    return `Hello my name is ${this.name} and i'm ${
-      this.age
-    } years old. ${this.#secretPin()}`
+  // Public method to display vehicle status
+  getStatus() {
+    return `Speed: ${this.#speed} km/h, Fuel: ${this.#fuel} liters`
   }
 
-  #secretPin() {
-    return `This is my secret pin: ${this.#pin}`
+  // Public method to drive the vehicle
+  drive(distance) {
+    const fuelNeeded = distance / 10
+    if (fuelNeeded <= this.#fuel) {
+      this.#fuel -= fuelNeeded
+      this.#speed += 10
+      console.log(`Drove ${distance} km. ${this.getStatus()}`)
+    } else {
+      console.log('Not enough fuel to drive the distance.')
+    }
+  }
+
+  // Public method to refuel the vehicle
+  refuel(amount) {
+    this.#fuel += amount
+    console.log(`Refueled ${amount} liters. ${this.getStatus()}`)
   }
 }
 
-const learner = new Learner('Anbu', 27, 1234)
+// Creating an instance of the Vehicle class
+const myCar = new Vehicle(60, 20)
 
-console.log(learner.bio())
-console.log(learner._protectedProperty)
+// Interacting with the vehicle using public methods
+myCar.drive(50) // Drove 50 km. Speed: 70 km/h, Fuel: 15 liters
+myCar.refuel(10) // Refueled 10 liters. Speed: 70 km/h, Fuel: 25 liters
+myCar.drive(200) // Not enough fuel to drive the distance.
+
+new Object()
