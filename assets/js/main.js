6d41a949be45ea558bdfa1b33e46afaa053f093b
Anbuselvan Rocky <15264938+anburocky3@users.noreply.github.com>
2024-11-22 02:04:03 +0530

    24-recorded

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,4 @@
-// Function prototype methods
-const numbers = [5, 2, 4, 7, 1]
+// Spread Operator uses in
+const myName = 'Anbuselvan'
 
-// console.log(Math.min(5, 2, 4, 7, 1))
-// console.log(Math.max(5, 2, 4, 7, 1))
-
-//  Function.prototype.apply()
-const result = Math.min.call(null, 5, 2, 4, 7, 1)
-const result2 = Math.min.apply(null, numbers)
-// Function.prototype.bind()
-
-const result3 = Math.min.bind(null, ...numbers)
-
-console.log('using call()', result)
-console.log('using apply()', result2)
-console.log('using bind()', result3())
+console.log(...myName)
