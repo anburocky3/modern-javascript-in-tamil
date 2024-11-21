c071c0207358649576079f4aad0930e9ef022ce9
Anbuselvan Rocky <15264938+anburocky3@users.noreply.github.com>
2024-11-22 02:41:10 +0530

    25 - ternary operator

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,8 @@
 function getStatusMessage(age, isMember) {
   return age >= 18
     ? isMember
-      ? 'Welcome, adult member!'
-      : 'Welcome, adult guest!'
+      ? 'Adult member'
+      : 'Adult Guest'
     : 'Access denied. Minors are not allowed.'
 }
 
@@ -10,6 +10,9 @@ const user1 = { age: 20, isMember: true }
 const user2 = { age: 20, isMember: false }
 const user3 = { age: 16, isMember: true }
 
-console.log(getStatusMessage(user1.age, user1.isMember)) // Output: Welcome, adult member!
-console.log(getStatusMessage(user2.age, user2.isMember)) // Output: Welcome, adult guest!
-console.log(getStatusMessage(user3.age, user3.isMember)) // Output: Access denied. Minors are not allowed.
+console.log(getStatusMessage(user1.age, user1.isMember))
+// Output: Welcome, adult member!
+console.log(getStatusMessage(user2.age, user2.isMember))
+// Output: Welcome, adult guest!
+console.log(getStatusMessage(user3.age, user3.isMember))
+// Output: Access denied. Minors are not allowed.
