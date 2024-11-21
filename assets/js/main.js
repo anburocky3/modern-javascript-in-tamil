c97cf1711f64fb29843420a2381dc00ff16ee341
Anbuselvan Rocky <15264938+anburocky3@users.noreply.github.com>
2024-11-21 13:07:09 +0530

    feat: ternary operator

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,15 @@
-// Spread Operator uses in
-const myName = 'Anbuselvan'
+function getStatusMessage(age, isMember) {
+  return age >= 18
+    ? isMember
+      ? 'Welcome, adult member!'
+      : 'Welcome, adult guest!'
+    : 'Access denied. Minors are not allowed.'
+}
 
-console.log(...myName)
+const user1 = { age: 20, isMember: true }
+const user2 = { age: 20, isMember: false }
+const user3 = { age: 16, isMember: true }
+
+console.log(getStatusMessage(user1.age, user1.isMember)) // Output: Welcome, adult member!
+console.log(getStatusMessage(user2.age, user2.isMember)) // Output: Welcome, adult guest!
+console.log(getStatusMessage(user3.age, user3.isMember)) // Output: Access denied. Minors are not allowed.
